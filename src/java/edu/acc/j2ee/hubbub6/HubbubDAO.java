e56abab7faf51ac899d5bc5e8c5a1e5486d8ceae
JC <JC@10.255.32.60>
2015-09-05 12:06:56 -0500

    new way of implementing back end tables

--- src/java/edu/acc/j2ee/hubbub6/HubbubDAO.java
+++ src/java/edu/acc/j2ee/hubbub6/HubbubDAO.java
@@ -32,7 +32,7 @@ public class HubbubDAO {
         try {
             pstat = CONN.prepareStatement(sql);
             pstat.setString(1, post.getContent());
-            pstat.setInt(2, post.getAuthor().getId());
+            pstat.setInt(2, post.getAuthor().getProfile().getId());
             pstat.setDate(3, new java.sql.Date(post.getPostDate().getTime()));
             pstat.executeUpdate();
             lastError = null;
@@ -58,8 +58,8 @@ public class HubbubDAO {
 
     public List<Post> getSortedPostsFor(String userName) {
         List<Post> posts = new ArrayList<>();
-        String sql = "SELECT * FROM Posts JOIN Users ON Posts.authorid = ";
-        sql += "Users.id WHERE username LIKE '%s' ORDER BY postdate DESC";
+        String sql = "SELECT * FROM Posts JOIN Profiles ON Posts.authorid = ";
+        sql += "Profiles.id JOIN users ON profiles.id = users.profileid WHERE username LIKE '%s' ORDER BY postdate DESC";
         sql = String.format(sql, userName);
         Statement stat = null;
         ResultSet rs = null;
@@ -92,7 +92,7 @@ public class HubbubDAO {
     }
     
     public User getUserById(int id) {
-        String sql = "SELECT * FROM USERS WHERE id = " + id;
+        String sql = "SELECT * FROM USERS WHERE profileid = " + id;
         Statement stat = null;
         ResultSet rs = null;
         User user = null;
@@ -101,7 +101,7 @@ public class HubbubDAO {
             rs = stat.executeQuery(sql);
             if (rs.next()) {
                 user = new User(rs.getString("username"));
-                user.setId(rs.getInt("id"));
+                user.getProfile().setId(rs.getInt("id"));
             }
             lastError = null;
         } catch (SQLException sqle) {
@@ -121,7 +121,7 @@ public class HubbubDAO {
     
     public Profile getProfileFor(int userId) {
         Profile profile = null;
-        String sql = "SELECT * FROM Profiles WHERE userid=" + userId;
+        String sql = "SELECT * FROM Profiles WHERE id=" + userId;
         Statement stat = null;
         ResultSet rs = null;
         try {
@@ -149,7 +149,7 @@ public class HubbubDAO {
 
 public Profile getProfileFor(String userName) {
         Profile profile = null;
-        String sql = "SELECT * FROM Profiles JOIN Users ON Profiles.userId = Users.id WHERE userName='%s'";
+        String sql = "SELECT * FROM Profiles JOIN Users ON Profiles.id = Users.profileid WHERE Users.username='%s'";
         sql = String.format(sql, userName);
         Statement stat = null;
         ResultSet rs = null;
@@ -187,7 +187,7 @@ public Profile getProfileFor(String userName) {
             rs = stat.executeQuery(sql);
             if (rs.next()) {
                 user = new User(rs.getString("username"));
-                user.setId(rs.getInt("id"));
+                user.getProfile().setId(rs.getInt("id"));
             }
             lastError = null;
         } catch (SQLException sqle) {
@@ -206,50 +206,42 @@ public Profile getProfileFor(String userName) {
     }
     
     public int register(RegistrationBean bean) {
-        String userSql = "INSERT INTO USERS (username,password) VALUES (?,?)";
+        
         String profSql = "INSERT INTO PROFILES (firstname,lastname,";
-        profSql += "email,zip,userid) VALUES (?,?,?,?,?)";
-        String updateSql = "UPDATE Users SET profileid = ? WHERE id = ?";
-        PreparedStatement pstatUser = null, pstatProf = null, pstatUpdate = null;
-        ResultSet userRs = null, profRs = null;
-        int id = 0, id2 = 0;
+        profSql += "email,zip) VALUES (?,?,?,?)";
+        String userSql = "INSERT INTO USERS (username,password, profileid) VALUES (?,?,?)";
+        //String updateSql = "UPDATE Users SET profileid = ? WHERE id = ?";
+        PreparedStatement pstatUser = null, pstatProf = null;
+        ResultSet profRs = null;
+        int id = 0;
         try {
-            pstatUser = CONN.prepareStatement(userSql, Statement.RETURN_GENERATED_KEYS);
-            pstatUser.setString(1, bean.getUserName());
-            pstatUser.setString(2, bean.getPassword1());
-            pstatUser.executeUpdate();
-            userRs = pstatUser.getGeneratedKeys();
-            if (userRs.next())
-                id = userRs.getInt(1);
             pstatProf = CONN.prepareStatement(profSql, Statement.RETURN_GENERATED_KEYS);
             pstatProf.setString(1, bean.getFirstName());
             pstatProf.setString(2, bean.getLastName());
             pstatProf.setString(3, bean.getEmail());
-            pstatProf.setString(4, bean.getZipCode());
-            pstatProf.setInt(5, id);
+            pstatProf.setString(4, bean.getZipCode());            
             pstatProf.executeUpdate();
             profRs = pstatProf.getGeneratedKeys();
+            
+            //profRs = pstatUser.getGeneratedKeys();
             if (profRs.next())
-                id2 = profRs.getInt(1);
-            pstatUpdate = CONN.prepareStatement(updateSql);
-            pstatUpdate.setInt(1, id2);
-            pstatUpdate.setInt(2, id);
-            pstatUpdate.executeUpdate();  
+                id = profRs.getInt(1);
+            pstatUser = CONN.prepareStatement(userSql, Statement.RETURN_GENERATED_KEYS);
+            pstatUser.setString(1, bean.getUserName());
+            pstatUser.setString(2, bean.getPassword1());
+            pstatUser.setInt(3, id);
+            pstatUser.executeUpdate();
             lastError = null;
         } catch (SQLException sqle) {
             lastError = sqle.getMessage();
         } finally {
-            if (userRs != null)
-                try { userRs.close(); } catch (SQLException sqle) {}
             if (profRs != null)
                 try { profRs.close(); } catch (SQLException sqle){}
             if (pstatUser != null)
                 try { pstatUser.close(); } catch (SQLException sqle) {}
             if (pstatProf != null)
                 try { pstatProf.close(); } catch (SQLException sqle) {}
-            if (pstatUpdate != null)
-                try { pstatUpdate.close(); } catch (SQLException sqle) {}
-        }
+            }
         return id;
     }
     
@@ -270,7 +262,7 @@ public Profile getProfileFor(String userName) {
         } 
         if (bean.getPassword1().length() > 0) {
             sql = "UPDATE Users SET password='" + bean.getPassword1() + "'";
-            sql += " WHERE id=" + user.getId();
+            sql += " WHERE profileid=" + user.getProfile().getId();
             try {
                 stat = CONN.createStatement();
                 stat.executeUpdate(sql);
